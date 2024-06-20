2eb8d75e30e63fd8649d886b53a1ceb7f74e2ffe
Luke B <115926691+lukebarnes-ct@users.noreply.github.com>
2024-06-20 13:59:29 +0200

    Attempting to Develop the Simulation Phase of the Model Development with Triple For Loops.

--- XU-ABM.jl
+++ XU-ABM.jl
@@ -2,6 +2,7 @@
 
 using Random
 using Plots
+using Distributions
 
 ### Parameters
 
@@ -37,6 +38,46 @@ dividends = zeros(N, T)         # Dividends of Risky Assets
 fund_val = zeros(N, T)          # Fundamental Values of Risky Assets
 price = zeros(N, T)             # Prices of Risky Assets
 
-expRet_Fund = zeros(N, T)       # Fundamentalists Expected Return of Risky Assets
-expRet_Chart = zeros(N, T)      # Chartists Expected Return of Risky Assets
-expRet_CovMat = zeros(N, N)     # Expected Return Covariance Matrix
+expRet_Fund = zeros(N, T, kFund)                    # Fundamentalists Expected Return of Risky Assets
+expRet_Chart = zeros(N, T, kChart)                  # Chartists Expected Return of Risky Assets
+expRet_CovMat_Fund = zeros(N, N, T, kFund)          # Expected Return Covariance Array for Fundamentalists
+expRet_CovMat_Chart = zeros(N, N, T, kChart)        # Expected Return Covariance Array for Chartists
+
+for i in 1:N
+    dividends[i, 1] = div_0       # Set Initial Dividend in Matrix
+end
+
+meanR = round.(rand(Uniform(meanR_min, meanR_max), kFund), digits = 2)
+ema_wind_Fund = rand(wind_min:wind_max, kFund)
+ema_wind_Chart = rand(wind_min:wind_max, kChart)
+
+
+for t in 2:T
+
+    covVec_Fund = zeros(N, kFund)
+    covVec_Chart = zeros(N, kChart)
+
+    for i in 1:N
+
+        err = rand(Normal(0, 1))                                                # Standard Normal Error Term
+        dividends[i, t] = (1 + phi + phi_sd * err) * dividends[i, t-1]          # Expected Dividends for Next Time Period
+        fund_val[i, t] = (1 + phi + phi_sd * err) * fund_val[i, t-1]            # Expected Fundamental Value for Next Time Period
+
+        for f in 1:kFund
+
+            expRet_Fund[i, t, f] = (phi * fund_val[i, t-1] + meanR[f] * (fund_val[i, t-1] - price[i, t-1]) + (1 + phi) * dividends[i, t-1] - price[i, t-1]) / price[i, t-1]
+
+            ema_f = exp(-1/ema_wind_Fund[f])
+            expRet_CovMat_Fund[i, i, t, f] = (ema_f * expRet_CovMat_Fund[i, i, t-1, f]) + (1 - ema_f) * (expRet_Fund[i, t-1, f] - ema_f)
+            
+
+        end
+        
+        for c in 1:kChart
+
+
+        end
+
+    end
+    
+end
